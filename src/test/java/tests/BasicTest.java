7b717dbceff524a4ab9c766f8f50beac40f3dc1e
Milos-Jovic <milosjovic392@gmail.com>
2023-10-19 20:01:30 +0200

    Test: Basic - Basic

---
+++ src/test/java/tests/BasicTest.java
@@ -0,0 +1,60 @@
+package tests;
+
+import helper.Helper;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import pages.*;
+import java.io.IOException;
+import java.time.Duration;
+
+public class BasicTest {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    protected String baseUrl = "https://vue-demo.daniel-avellaneda.com";
+    protected NavPage navPage;
+    protected LoginPage loginPage;
+    protected MessagePopUpPage messagePopUpPage;
+    protected SignUpPage signUpPage;
+    protected CitiesPage citiesPage;
+
+
+    @BeforeClass
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        navPage = new NavPage(driver, wait);
+        loginPage = new LoginPage(driver, wait);
+        messagePopUpPage = new MessagePopUpPage(driver, wait);
+        signUpPage = new SignUpPage(driver, wait);
+        citiesPage = new CitiesPage(driver, wait);
+
+
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        driver.navigate().to(baseUrl);
+    }
+    @AfterMethod
+    public void afterMethod(ITestResult testResult) throws IOException {
+        if (testResult.getStatus() == ITestResult.FAILURE) {
+            Helper.getScreenshotInTimeFormat(driver, testResult.getName());
+        }
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+}
