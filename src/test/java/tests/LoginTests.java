3fe9c9186eb4d043db856d58019329e4f5b1d2ac
Milos-Jovic <milosjovic392@gmail.com>
2023-10-19 20:06:12 +0200

    Test: Login - SuccessfulLogin

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -62,20 +62,20 @@ public class LoginTests extends BasicTest {
                 "Current url should be " + baseUrl + "/login");
 
     }
-//    @Test(priority = 5, retryAnalyzer = RetryAnalyzer.class)
-//    public void SuccessfulLogin() {
-//
-//        String email = "admin@admin.com";
-//        String password = "12345";
-//
-//        navPage.clickOnLoginButton();
-//        navPage.waitUntilCurrentUrlContainsLogin();
-//        loginPage.fillInEmailInputField(email);
-//        loginPage.fillInPasswordInputField(password);
-//        loginPage.clickOnLoginButton();
-//        navPage.waitUntilCurrentUrlContainsHome();
-//
-//    }
+    @Test(priority = 5, retryAnalyzer = RetryAnalyzer.class)
+    public void SuccessfulLogin() {
+
+        String email = "admin@admin.com";
+        String password = "12345";
+
+        navPage.clickOnLoginButton();
+        navPage.waitUntilCurrentUrlContainsLogin();
+        loginPage.fillInEmailInputField(email);
+        loginPage.fillInPasswordInputField(password);
+        loginPage.clickOnLoginButton();
+        navPage.waitUntilCurrentUrlContainsHome();
+
+    }
 //    @Test(priority = 6, retryAnalyzer = RetryAnalyzer.class)
 //    public void Logout() {
 //
