58f358b18bfe416f489fcad962af8c6b7127bd01
Milos-Jovic <milosjovic392@gmail.com>
2023-10-19 19:59:08 +0200

    Test: AuthRoutes - ForbidsVisitsToHomeUrlIfNotAuthenticated

---
+++ src/test/java/tests/AuthRoutesTests.java
@@ -0,0 +1,42 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import retry.RetryAnalyzer;
+
+public class AuthRoutesTests extends BasicTest {
+
+    @Test(priority = 1, retryAnalyzer = RetryAnalyzer.class)
+    public void ForbidsVisitsToHomeUrlIfNotAuthenticated() {
+
+        driver.navigate().to(baseUrl + "/home");
+        Assert.assertEquals(navPage.getCurrentUrl(), baseUrl + "/login",
+                "Current url should be " + baseUrl + "/login");
+
+    }
+
+//    @Test(priority = 2, retryAnalyzer = RetryAnalyzer.class)
+//    public void ForbidsVisitsToProfileUrlIfNotAuthenticated() {
+//
+//        driver.navigate().to(baseUrl + "/profile");
+//        Assert.assertEquals(navPage.getCurrentUrl(), baseUrl + "/login",
+//                "Current url should be " + baseUrl + "/login");
+//    }
+//
+//    @Test(priority = 3, retryAnalyzer = RetryAnalyzer.class)
+//    public void ForbidsVisitsToAdminCitiesUrlIfNotAuthenticated() {
+//
+//        driver.navigate().to(baseUrl + "/admin/cities");
+//        Assert.assertEquals(navPage.getCurrentUrl(), baseUrl + "/login",
+//                "Current url should be " + baseUrl + "/login");
+//
+//    }
+//    @Test(priority = 4, retryAnalyzer = RetryAnalyzer.class)
+//    public void  ForbidsVisitsToAdminUsersUrlIfNotAuthenticated() {
+//
+//        driver.navigate().to(baseUrl + "/admin/users");
+//        Assert.assertEquals(navPage.getCurrentUrl(), baseUrl + "/login",
+//                "Current url should be " + baseUrl + "/login");
+//
+//    }
+//}
