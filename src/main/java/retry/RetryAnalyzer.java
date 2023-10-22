bf95f041112e7ba0ddb18a26c5c1a58da474fac9
Milos-Jovic <milosjovic392@gmail.com>
2023-10-22 16:20:32 +0200

    Retry - RetryAnalyzer

--- src/main/java/retry/RetryAnalyzer.java
+++ src/main/java/retry/RetryAnalyzer.java
@@ -5,7 +5,7 @@ import org.testng.ITestResult;
 
 public class RetryAnalyzer implements IRetryAnalyzer {
     private int count = 0;
-    private int maxCount = 2;
+    private int maxCount = 3;
 
     @Override
     public boolean retry(ITestResult iTestResult) {
