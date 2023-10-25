dfff86f2aa18b89a6756b650a8aa60afc1140e89
Milos-Jovic <milosjovic392@gmail.com>
2023-10-25 11:32:50 +0200

    pages: SignUpPage

--- src/main/java/pages/SignUpPage.java
+++ src/main/java/pages/SignUpPage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+
 public class SignUpPage extends BasicPage {
     public SignUpPage(WebDriver driver, WebDriverWait wait) {
         super(driver, wait);
